e221c635ed04685f643946e43aedd0014cffbf6f
ChAliraza523 <choudhryaliraza7160@gmail.com>
2021-06-15 14:50:56 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import NavBar from './Components/NavBar';
+import Grid from './Components/Grid';
+function App() {
+	return (
+		<div>
+			<NavBar/>
+			<Grid/>
+
+		</div>
+	)
+}
+export default App;
